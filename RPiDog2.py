b8921430ef06ff8f2b4c34928a056bdd1f28a552
YousefRepository <elhassay@mcmaster.ca>
2026-09-10 23:50:53 -0400

    Update RPiDog2.py functions

--- RPiDog2.py
+++ RPiDog2.py
@@ -3,14 +3,22 @@
 import math
 import time
 from adafruit_servokit import ServoKit #was programmed on the RPi where it has the libraries installed
-
-
 kit = ServoKit(channels=16) 
+
+euler = 2.7183
 
 def move_servo(channel, angle):
     kit.servo[channel].angle = angle
 
-euler = 2.7183
+def angleCalc(x, y):
+    angle = math.degrees(math.atan(x/y)) 
+    c = math.sqrt(x**2 + y**2)
+    Bangle = math.degrees(math.acos((a**2 + c**2 - b**2)/(2*a*c)))
+    Cangle = math.degrees(math.acos((a**2+b**2-c**2)/(2*a*b)))
+    offsetFunction = float(181.9*(euler)**(-0.2929*c))
+    move_servo(13, 180-Cangle) #default
+    move_servo(12, Bangle-offsetFunction-angle) #default
+    time.sleep(delay)
 
 move_servo(14, 115) #default configuration
 move_servo(13, 90) #default configuration
@@ -28,47 +36,13 @@ delay=0.2
 
 for z in range(0,20): # draw rectangle 10 times
     x += 5;
-    
-    angle = math.degrees(math.atan(x/y)) #should have made this a function, taken x and y as parameters, but this was for quick testing
-    c = math.sqrt(x**2 + y**2)
-    Bangle = math.degrees(math.acos((a**2 + c**2 - b**2)/(2*a*c)))
-    Cangle = math.degrees(math.acos((a**2+b**2-c**2)/(2*a*b)))
-    offsetFunction = float(181.9*(euler)**(-0.2929*c))
-    move_servo(13, 180-Cangle) #default
-    move_servo(12, Bangle-offsetFunction-angle) #default
-    time.sleep(delay)
+    angleCalc(x, y)
     
     y += 8;
-    
-    angle = math.degrees(math.atan(x/y)) 
-    c = math.sqrt(x**2 + y**2)
-    Bangle = math.degrees(math.acos((a**2 + c**2 - b**2)/(2*a*c)))
-    Cangle = math.degrees(math.acos((a**2+b**2-c**2)/(2*a*b)))
-    offsetFunction = float(181.9*(euler)**(-0.2929*c))
-    move_servo(13, 180-Cangle) #default
-    move_servo(12, Bangle-offsetFunction-angle) #default
-    time.sleep(delay)
+    angleCalc(x, y)
     
     x -= 5;
-    
-    angle = math.degrees(math.atan(x/y)) 
-    c = math.sqrt(x**2 + y**2)
-    Bangle = math.degrees(math.acos((a**2 + c**2 - b**2)/(2*a*c)))
-    Cangle = math.degrees(math.acos((a**2+b**2-c**2)/(2*a*b)))
-    offsetFunction = float(181.9*(euler)**(-0.2929*c))
-    move_servo(13, 180-Cangle) #default
-    move_servo(12, Bangle-offsetFunction-angle) #default
-    time.sleep(delay)
+    angleCalc(x, y)
     
     y -= 8;
-    
-    angle = math.degrees(math.atan(x/y)) 
-    c = math.sqrt(x**2 + y**2)
-    Bangle = math.degrees(math.acos((a**2 + c**2 - b**2)/(2*a*c)))
-    Cangle = math.degrees(math.acos((a**2+b**2-c**2)/(2*a*b)))
-    offsetFunction = float(181.9*(euler)**(-0.2929*c))
-    move_servo(13, 180-Cangle) #default
-    move_servo(12, Bangle-offsetFunction-angle) #default
-    time.sleep(delay)
-
-
+    angleCalc(x, y)
